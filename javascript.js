fb873fa03736943a9a865db0c228fa12ad535e76
cannicombustiva <salvatoredifranco8@gmail.com>
2017-01-14 18:58:26 +0100

    Added quotes

---
+++ javascript.js
@@ -0,0 +1,7 @@
+var quotes = [
+  "Ormai è ufficiale, Segni è un coglione (5/1/1994)",
+  "Io non farò il politico come gli altri (28/1/1994)",
+  "Rinuncerei allo scudetto pur di vincere le elezioni (24/3/1994)",
+  "In una certa fase Mussolini fece cose buone, un fatto confermato dalla storia (28/5/1994)",
+  "Non perdo tempo a smentire sciocchezze. Anzi, proporrò una tassa sulle chiacchiere (13/8/1994)"
+]
